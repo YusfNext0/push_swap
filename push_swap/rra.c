e8e029bd983ec5c55216468aa48abeca0a47dddb
YOUSSEF NACHAT <105310735+YusfNext0@users.noreply.github.com>
2024-03-01 13:06:34 +0100

    Add files via upload

---
+++ push_swap/rra.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rra.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ynachat <ynachat@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/27 09:26:05 by ynachat           #+#    #+#             */
+/*   Updated: 2024/02/27 10:22:48 by ynachat          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void    rra(p_list **a)
+{
+    p_list *last;
+    p_list *befor;
+
+    befor = (*a);
+    last = lstlast((*a));
+    while (befor->next->next)
+       befor = befor->next;
+    befor->next = NULL;
+    last->next = (*a);
+    (*a) = last;
+}
